19f233b1b7f671ca8c2f1b7d7d86ef741012f7c1
3DExtended <15470335+3DExtended@users.noreply.github.com>
2026-01-07 15:48:23 +0100

    init

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# install xcode and git and prerequisits for brew
+xcode-select --install
+
+# download and setup brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /Users/$USER/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# vscode
+brew install --cask visual-studio-code
+
+
+# install flutter for app development
+brew install flutter
+flutter doctor
+
+# install android studio for android builds
+brew install --cask android-studio
+
+brew install --cask docker
